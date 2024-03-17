e8ae1850551af8df813c8082f850dbc7db8a243e
rohitar19 <rohit960211@gmail.com>
2024-03-18 00:57:38 +0530

    Depolyed on AWS

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
+const password =encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+const connectionString=`mongodb+srv://rohit960211:${password}@cluster0.qfbl9nq.mongodb.net/Chat?retryWrites=true&w=majority&appName=Cluster0`
+
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
+		await mongoose.connect(connectionString);
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
